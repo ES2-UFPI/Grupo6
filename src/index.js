7038c8ce02129c2605bf2feae8fa4bd8a9d359a6
Helder Sérvio <41337686+heldersrvio@users.noreply.github.com>
2021-05-31 12:41:58 -0300

    Revert "Helder#9"

--- src/index.js
+++ src/index.js
@@ -1,41 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import Reducer from './Reducers/Reducer';
-
+import './index.css';
 import App from './App';
-import RegisterProductPage from './Visualization/Components/Products/RegisterProductPage';
-import HomePageMockup from './Visualization/Components/Navigation/HomePageMockup';
-import ProductPageMockup from './Visualization/Components/Products/ProductPageMockup';
-import CartPage from './Visualization/Components/Products/CartPage';
-import Header from './Visualization/Components/Main/Header';
-import FootBar from './Visualization/Components/Main/FootBar';
-import CategoryPage from './Visualization/Components/Navigation/CategoryPage';
-import ProductPage from './Visualization/Components/Products/ProductPage';
-
-const store = createStore(
-	combineReducers({ user: Reducer.userReducer, cart: Reducer.cartReducer })
-);
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<BrowserRouter>
-				<Header />
-				<Switch>
-					<Route path="/product/category/:category" component={CategoryPage} />
-					<Route path="/product/add" component={RegisterProductPage} />
-					<Route path="/product/:productId" component={ProductPage} />
-					<Route path="/homepage" component={HomePageMockup} />
-					<Route path="/productMockup" component={ProductPageMockup} />
-					<Route path="/shoppingCart" component={CartPage} />
-					<Route path="/" component={App} />
-				</Switch>
-				<FootBar />
-			</BrowserRouter>
-		</Provider>
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
