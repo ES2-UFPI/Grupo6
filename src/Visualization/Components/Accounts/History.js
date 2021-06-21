2310144b19aa934088408109c2ec9c09f57e18d8
Helder Sérvio <heldersrvio@gmail.com>
2021-06-21 13:54:47 -0300

    Expanded TransactionItem and imported it into History

--- src/Visualization/Components/Accounts/History.js
+++ src/Visualization/Components/Accounts/History.js
@@ -1,63 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 import '../Styles/History.css';
+import TransactionItem from './TransactionItem';
 
 const History = () => {
+	const items = Array(5).fill({
+		id: '1234',
+		buyerId: '4321',
+		sellerId: '0123',
+		sellerName: 'Jô',
+		productName: 'produto',
+		localizationCode: 'localizationCode',
+		productPicture:
+			'https://images.pexels.com/photos/2520829/pexels-photo-2520829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+		status: '¯|_(ツ)_/¯',
+		rating: '3.5',
+		wouldBarterAgain: 'y/n',
+	});
 
-    const items = Array(5).fill({
-            id: '1234',
-            buyerId: '4321',
-            sellerId: '0123',
-            sellerName: 'Jô',
-            productName: 'produto',
-            localizationCode: 'localizationCode',
-            productPicture: 'https://images.pexels.com/photos/2520829/pexels-photo-2520829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-            status: "¯|_(ツ)_/¯",
-            rating: '3.5',
-            wouldBarterAgain: 'y/n'
-        })
-    
-    const [inspec, setInspec] = useState(items[0])
-    
-    const details = (
-        <div id='dMain' className="details-main">
-            <div className="details-close"><button onClick={()=>{document.getElementById('dMain').style.visibility = "hidden"}}></button></div>
-            <div className="details-img">
-                <img src={inspec.productPicture} alt="product"/>
-            </div>
-            <div>
-                <label>cod: {inspec.id}</label>
-            </div>
-            <div><label>would barter again? {inspec.wouldBarterAgain}</label></div>
-            <div><label>Avaliação: {inspec.rating}</label></div>
-        </div>
-    )
-    
-    return (
-        <div className="history-main">
-            <h2 className="history-title">Histórico de Transações</h2>
-            <div className="transation-items">
-                {items.map((i, index) => {
-                    return (
-                        <div className="transation-item-info">
-                            <div className="left">
-                                <h2>{i.productName}</h2>
-                                <div><label>Vendedor: {i.sellerName}</label></div>
-                                <div><label>Localizar: {i.localizationCode}</label></div>
-                                <div><label>Status: {i.status}</label></div>
-                            </div>
-                            <div className="right">
-                                <button onClick={()=>{
-                                    document.getElementById('dMain').style.visibility = "visible"
-                                    setInspec(i)}}>Inspecionar</button>
-                            </div>
-                        </div>
-                    )
-                })}  
-            </div>
-            <div>{details}</div>
-        </div>
-    )
-}
+	return (
+		<div className="history-main">
+			<h2 className="history-title">Histórico de Transações</h2>
+			<div className="transation-items">
+				{items.map((i, index) => {
+					return <TransactionItem {...i} />;
+				})}
+			</div>
+		</div>
+	);
+};
 
 export default History;
--- src/Visualization/Components/Accounts/TransactionItem.js
+++ src/Visualization/Components/Accounts/TransactionItem.js
@@ -3,33 +3,83 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const TransactionItem = (props) => {
-    const [selectedRating, setSelectedRating] = useState(props.rating)
-    
-    return (
-        <div className="transaction-item">
-            <div className="transaction-item-left-section">
-                <img src={props.productPicture} alt={props.productName} />
-            </div>
-            <div className="transaction-item-right-section">
-                <span className="transaction-item-name">{props.productName}</span>
-                <Link to={`/account/${props.sellerId}`} className="seller-name-link">{`Vendedor: ${props.sellerName}`}</Link>
-            </div>
-        </div>
-    );
+	const [selectedRating, setSelectedRating] = useState(props.rating);
+
+	const details = (
+		<div id="dMain" className="details-main">
+			<div className="details-close">
+				<button
+					onClick={() => {
+						document.getElementById('dMain').style.visibility = 'hidden';
+					}}
+				></button>
+			</div>
+			<div className="details-img">
+				<img src={props.productPicture} alt="product" />
+			</div>
+			<div>
+				<label>cod: {props.id}</label>
+			</div>
+			<div>
+				<label>would barter again? {props.wouldBarterAgain}</label>
+			</div>
+			<div>
+				<label>Avaliação: {props.rating}</label>
+			</div>
+		</div>
+	);
+
+	return (
+		<div className="transaction-item">
+			<div className="transaction-item-left-section">
+				<img src={props.productPicture} alt={props.productName} />
+			</div>
+			<div className="transaction-item-right-section">
+				<span className="transaction-item-name">{props.productName}</span>
+				<Link
+					to={`/account/${props.sellerId}`}
+					className="seller-name-link"
+				>{`Vendedor: ${props.sellerName}`}</Link>
+				<div className="transation-item-info">
+					<div className="left">
+						<h2>{props.productName}</h2>
+						<div>
+							<label>Vendedor: {props.sellerName}</label>
+						</div>
+						<div>
+							<label>Localizar: {props.localizationCode}</label>
+						</div>
+						<div>
+							<label>Status: {props.status}</label>
+						</div>
+					</div>
+					<div className="right">
+						<button
+							onClick={() => {
+								document.getElementById('dMain').style.visibility = 'visible';
+							}}
+						>
+							Inspecionar
+						</button>
+					</div>
+					{details}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 TransactionItem.propTypes = {
-    transactionId: PropTypes.string,
-    buyerId: PropTypes.string,
-    sellerId: PropTypes.string,
-    sellerName: PropTypes.string,
-    productName: PropTypes.string,
-    localizationCode: PropTypes.string,
-    productPicture: PropTypes.string,
-    status: PropTypes.string,
-    rating: PropTypes.number,
-    wouldBarterAgain: PropTypes.bool,
-
+	transactionId: PropTypes.string,
+	buyerId: PropTypes.string,
+	sellerId: PropTypes.string,
+	sellerName: PropTypes.string,
+	productName: PropTypes.string,
+	localizationCode: PropTypes.string,
+	productPicture: PropTypes.string,
+	status: PropTypes.string,
+	rating: PropTypes.number,
+	wouldBarterAgain: PropTypes.bool,
 };
 
 export default TransactionItem;
