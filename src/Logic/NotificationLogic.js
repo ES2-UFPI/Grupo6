03e4470e92aee2de9bdefdf5dbf53c41400f53a4
Helder Sérvio <heldersrvio@gmail.com>
2021-06-26 15:32:33 -0300

    Added setNotificationAsRead method to NotificationLogic

--- src/Logic/NotificationLogic.js
+++ src/Logic/NotificationLogic.js
@@ -1,15 +1,20 @@
 import Firebase from '../Data/Firebase';
 
 const NotificationLogic = (() => {
+	const fetchUserNotifications = async (userId) => {
+		return (await Firebase.getAllNotifications())
+			.filter((notification) => notification.userId === userId)
+			.sort((a, b) => a.date.toDate() - b.date.toDate());
+	};
 
-    const fetchUserNotifications = async (userId) => {
-        return (await Firebase.getAllNotifications()).filter((notification) => notification.userId === userId).sort((a, b) => a.date.toDate() - b.date.toDate());
+    const setNotificationAsRead = async (notificationId) => {
+        await Firebase.setNotificationIsRead(true);
     };
 
-    return {
-        fetchUserNotifications,
-    };
-
+	return {
+		fetchUserNotifications,
+        setNotificationAsRead,
+	};
 })();
 
 export default NotificationLogic;
