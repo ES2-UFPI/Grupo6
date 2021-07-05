a78b9805d02c48ebbe647c291f474de8f6917c07
Helder Sérvio <heldersrvio@gmail.com>
2021-07-05 14:21:26 -0300

    Changed product page layout

--- src/Visualization/Components/Products/ProductPage.js
+++ src/Visualization/Components/Products/ProductPage.js
@@ -72,89 +72,92 @@ const ProductPage = ({ match }) => {
 
 	return isLoaded ? (
 		<div className="product-page">
-			<div className="left-section">
-				<img
-					src={productInfo.pictures.length > 0 ? productInfo.pictures[0] : ''}
-					alt={productInfo.name}
-				/>
-				<div className="product-details">
-					<h2 className="product-name-h2">{productInfo.name}</h2>
-					<p>{productInfo.description}</p>
-				</div>
-			</div>
-			<div className="right-section">
-				<div className="buying-area">
-					<div className="product-price-area">
-						<label htmlFor="product-price">Valor:</label>
-						<span className="product-price">{'R$ ' + price.toFixed(2)}</span>
+			<div className="top-section">
+				<div className="left-section">
+					<img
+						src={productInfo.pictures.length > 0 ? productInfo.pictures[0] : ''}
+						alt={productInfo.name}
+					/>
+					<div className="product-details">
+						<h2 className="product-name-h2">{productInfo.name}</h2>
+						<p>{productInfo.description}</p>
 					</div>
-					{!isItemInCartSelector ? (
+				</div>
+				<div className="right-section">
+					<div className="buying-area">
+						<div className="product-price-area">
+							<label htmlFor="product-price">Valor:</label>
+							<span className="product-price">{'R$ ' + price.toFixed(2)}</span>
+						</div>
+						{!isItemInCartSelector ? (
+							<button
+								className="add-to-cart-button"
+								onClick={(_e) =>
+									dispatch(
+										Reducer.addItem({
+											id: productId,
+											...productInfo,
+										})
+									)
+								}
+							>
+								Adicionar ao carrinho
+							</button>
+						) : (
+							<button
+								className="remove-from-cart-button"
+								onClick={(_e) => dispatch(Reducer.removeItem(productId))}
+							>
+								Remover do carrinho
+							</button>
+						)}
+					</div>
+					<input
+						type="text"
+						value={couponInput}
+						onChange={(e) => setCouponInput(e.target.value)}
+						placeholder="Cupom de desconto"
+					></input>
+					<div className="seller-area">
+						<div className="seller-area-top-section">
+							<img src={sellerInfo.profilePicture} alt={sellerInfo.name} />
+						</div>
+						<div className="seller-area-bottom-section">
+							<span className="seller-name-span">
+								{sellerInfo.name + ' ' + sellerInfo.surname}
+							</span>
+							<div className="seller-total-sales-area">
+								<label htmlFor="total-sales">Total de vendas: </label>
+								<span name="total-sales">{sellerInfo.numberOfSales}</span>
+							</div>
+							{sellerInfo.numberOfSales > 0 ? (
+								<div className="seller-rating-area">
+									<label htmlFor="rating">Avaliação média: </label>
+									<span name="rating">{sellerInfo.averageRating.toFixed(1)}</span>
+								</div>
+							) : null}
+							{sellerInfo.numberOfSales > 0 ? (
+								<div className="seller-would-barter-again-area">
+									<span className="seller-would-barter-again-span">{`${
+										sellerInfo.percentageWouldBarterAgain * 100
+									}% dos compradores fariam negócio novamente`}</span>
+								</div>
+							) : null}
+						</div>
 						<button
-							className="add-to-cart-button"
+							className="talk-to-seller-button"
 							onClick={(_e) =>
 								dispatch(
-									Reducer.addItem({
-										id: productId,
-										...productInfo,
-									})
+									Reducer.openChat(productInfo.sellerId, productInfo.sellerName)
 								)
 							}
 						>
-							Adicionar ao carrinho
+							Falar com o vendedor
 						</button>
-					) : (
-						<button
-							className="remove-from-cart-button"
-							onClick={(_e) => dispatch(Reducer.removeItem(productId))}
-						>
-							Remover do carrinho
-						</button>
-					)}
-				</div>
-				<input
-					type="text"
-					value={couponInput}
-					onChange={(e) => setCouponInput(e.target.value)}
-					placeholder="Cupom de desconto"
-				></input>
-				<div className="seller-area">
-					<div className="seller-area-top-section">
-						<img src={sellerInfo.profilePicture} alt={sellerInfo.name} />
 					</div>
-					<div className="seller-area-bottom-section">
-						<span className="seller-name-span">
-							{sellerInfo.name + ' ' + sellerInfo.surname}
-						</span>
-						<div className="seller-total-sales-area">
-							<label htmlFor="total-sales">Total de vendas: </label>
-							<span name="total-sales">{sellerInfo.numberOfSales}</span>
-						</div>
-						{sellerInfo.numberOfSales > 0 ? (
-							<div className="seller-rating-area">
-								<label htmlFor="rating">Avaliação média: </label>
-								<span name="rating">{sellerInfo.averageRating.toFixed(1)}</span>
-							</div>
-						) : null}
-						{sellerInfo.numberOfSales > 0 ? (
-							<div className="seller-would-barter-again-area">
-								<span className="seller-would-barter-again-span">{`${
-									sellerInfo.percentageWouldBarterAgain * 100
-								}% dos compradores fariam negócio novamente`}</span>
-							</div>
-						) : null}
-					</div>
-					<button
-						className="talk-to-seller-button"
-						onClick={(_e) =>
-							dispatch(
-								Reducer.openChat(productInfo.sellerId, productInfo.sellerName)
-							)
-						}
-					>
-						Falar com o vendedor
-					</button>
 				</div>
 			</div>
+			<div className="bottom-section"></div>
 		</div>
 	) : null;
 };
