13a0cff21f5681fa37a9d67f6bd49efa067b7f8c
Uriel Mori <68873798+Mori-Uri@users.noreply.github.com>
2021-07-18 22:40:16 -0300

    Nada

--- src/Logic/Tests/DoubtLogic.test.js
+++ src/Logic/Tests/DoubtLogic.test.js
@@ -6,7 +6,7 @@ test('doubts working properly', (done) => {
         const doubtId = await DoubtLogic.postDoubt('TIU6CY0xhB9ZPZqpK36U', '23', 'Tem quanto tempo de uso esse produto?');
         const doubt = await DoubtLogic.getDoubt(doubtId);
 
-        expect(doubt.message).toBe('Tem quanto tempo de uso esse produto?');
+        expect(doubt.question).toBe('Tem quanto tempo de uso esse produto?');
         done();
     }
 
